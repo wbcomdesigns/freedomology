d4e874e067f2e23a69f7b1c2b5124aa3294d7c51
Varun Kumar Dubey <vapvarun@gmail.com>
2025-02-10 20:41:03 +0530

    Update freedomology.php

--- plugins/freedomology-core/freedomology.php
+++ plugins/freedomology-core/freedomology.php
@@ -47,14 +47,56 @@ class Freedomology {
      * Initialize hooks
      */
     private function init_hooks() {
-        add_action( 'init', [ $this, 'custom_init' ] );
+        add_action( 'init', [ $this, 'initialize_plugin_features' ] );
+
+        if ( class_exists('GFForms') && class_exists('uncanny_learndash_groups\ProcessManualGroup') ) {
+            add_action('gform_after_submission_1', [ $this, 'ghl_learning_network_create_group_form_1' ], 10, 2);
+        }
     }
 
     /**
-     * Custom initialization function
+     * Initialize plugin features
+     *
+     * This function is responsible for setting up custom functionalities
+     * and loading any additional resources required by the plugin.
      */
-    public function custom_init() {
-        // Custom initialization code goes here
+    public function initialize_plugin_features() {
+        // Add custom post types, taxonomies, or other initialization code here.
+    }
+
+    /**
+     * Create a Group Using Gravity Form via Uncanny LearnDash Groups Plugin
+     */
+    public function ghl_learning_network_create_group_form_1($entry, $form) {
+        $first_name   = rgar($entry, '1');
+        $last_name    = rgar($entry, '3');
+        $email        = rgar($entry, '4');
+        $phone_number = rgar($entry, '5');
+        $course_id    = rgar($entry, '7');
+        $start_date   = rgar($entry, '8');
+        $total_seats  = rgar($entry, '11');
+        $group_name   = rgar($entry, '9');
+        $group_image  = '';
+
+        $customer_id = '';
+        if ( is_user_logged_in() ) {
+            $customer    = wp_get_current_user();
+            $customer_id = $customer->ID;
+        }
+
+        $args = array(
+            'ulgm_group_leader_first_name' => $first_name,
+            'ulgm_group_leader_last_name'  => $last_name,
+            'ulgm_group_leader_email'      => $email,
+            'ulgm_group_name'              => $group_name,
+            'ulgm_group_total_seats'       => $total_seats,
+            'ulgm_group_courses'           => [$course_id],
+            'ulgm_group_image'             => $group_image,
+            'ulgm_group_customer_id'       => $customer_id,
+        );
+
+        add_filter('ulgm_filter_var_is_front_end', function(){ return 'yes'; });
+        $group_id = \uncanny_learndash_groups\ProcessManualGroup::process( $args, $_POST );
     }
 }
 
